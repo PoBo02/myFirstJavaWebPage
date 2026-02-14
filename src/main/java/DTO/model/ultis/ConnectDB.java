139a88c9b5a08e79d6da7c7eac1e6e0d9f7b018b
PoBo02 <paopaopeo0258@gmail.com>
2026-02-14 20:55:08 +0700

    Sevlet

---
+++ src/main/java/DTO/model/ultis/ConnectDB.java
@@ -0,0 +1,38 @@
+package DTO.model.ultis;
+
+import javax.servlet.ServletContext;
+
+public class ConnectDB {
+
+    private String hostName;
+    private String port;
+    private String dbName;
+    private String userName;
+    private String password;;
+
+    public ConnectDB() {q
+            this.hostName = "localhost";
+            this.port = "1433";
+            this.dbName ="ProductIntro";
+            this.userName = "sa";
+            this.password ="12345";
+    }
+
+    public ConnectDB(ServletContext sc) {
+        this.hostName = sc.getInitParameter("hostAddress");
+
+        this.port = sc.getInitParameter("port");
+        this.dbName = sc.getInitParameter("dbName");
+        this.userName = sc.getInitParameter("userName");
+        this.password = sc.getInitParameter("userPass");
+    }
+    public String getUrlString() {
+        String fm = "jdbc:sqlserver://%s:%s;databaseName=%s;user=%s;password=%s";
+        return String.format(fm, this.hostName, this.port, this.dbName, this.userName, this.password);
+    }
+    public getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        return DriverManager.getConnection(this.getUrlString());
+    }
+
+}
