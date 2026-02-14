c54e3349baf2756159be71171c56e896788a01c6
PoBo02 <paopaopeo0258@gmail.com>
2026-02-14 20:08:36 +0700

    Account

---
+++ src/main/java/DTO/Account.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DTO;
+
+/**
+ *
+ * @author Ad
+ */
+public class Account {
+   
+   
+    private String account;
+    private String pass;
+    private String lastName;
+    private String FristName;
+    private Date birthday;
+    private boolean gender;
+    private String phone;
+    private boolean isUsed;
+    private int roleSystem;
+
+    public Account() {
+    }
+
+    public Account(String account, String pass, String lastName, String FristName, Date birthday, boolean gender,
+            String phone, boolean isUsed, int roleInSystem) {
+        this.account = account;
+        this.pass = pass;
+        this.lastName = lastName;
+        this.FristName = FristName;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.phone = phone;
+        this.isUsed = isUsed;
+        this.roleInSystem = roleInSystem;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFristName() {
+        return FristName;
+    }
+
+    public void setFristName(String FristName) {
+        this.FristName = FristName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isIsUsed() {
+        return isUsed;
+    }
+
+    public void setIsUsed(boolean isUsed) {
+        this.isUsed = isUsed;
+    }
+
+    public int getRoleInSystem() {
+        return roleInSystem;
+    }
+
+    public void setRoleInSystem(int roleInSystem) {
+        this.roleInSystem = roleInSystem;
+    }
+
+}
