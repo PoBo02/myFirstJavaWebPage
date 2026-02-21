4c10983ac5895dd161f2ee1271feaebc6c167c7d
PoBo02 <paopaopeo0258@gmail.com>
2026-02-21 18:52:14 +0700

    Half Function Finished

--- src/main/java/DTO/model/dao/AccountDAO.java
+++ src/main/java/DTO/model/dao/AccountDAO.java
@@ -1,7 +1,10 @@
 package DTO.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
@@ -28,22 +31,112 @@ public class AccountDAO implements Accessable<Account> {
 
     @Override
     public int insertRec(Account obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "INSERT INTO Account VALUES(?,?,?,?,?,?,?,?,?)";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, obj.getAccount());
+            ps.setString(2, obj.getPass());
+            ps.setString(3, obj.getLastName());
+            ps.setString(4, obj.getFirstName());
+            ps.setDate(5, obj.getBirthday());
+            ps.setBoolean(6, obj.isGender());
+            ps.setString(7, obj.getPhone());
+            ps.setBoolean(8, obj.isIsUsed());
+            ps.setInt(9, obj.getRoleInSystem());
+            int kq = ps.executeUpdate();
 
+            if (kq != 0) {
+                return kq;
+
+            } else {
+                throw new  SQLException();
+            }
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
     public int updateRec(Account obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "UPDATE Account SET pass=?, lastName=?, firstName=?, birthday=?, gender=?, phone=?, isUsed=?, roleInSystem=? WHERE account=?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, obj.getPass());
+            ps.setString(2, obj.getLastName());
+            ps.setString(3, obj.getFirstName());
+            ps.setDate(4, obj.getBirthday());
+            ps.setBoolean(5, obj.isGender());
+            ps.setString(6, obj.getPhone());
+            ps.setBoolean(7, obj.isIsUsed());
+            ps.setInt(8, obj.getRoleInSystem());
+            ps.setString(9, obj.getAccount());
+
+            int kq = ps.executeUpdate();
+
+            if (kq != 0) {
+                return kq;
+
+            } else {
+                throw new SQLException();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
+    //
     @Override
     public int deleteRec(Account obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "Delete from Account Where account=?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, obj.getAccount());
+            int kq = ps.executeUpdate();
+
+            if (kq != 0) {
+                return kq;
+
+            } else {
+                throw new Exception();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public List<Account> listAllByRole(int role) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        List<Account> list = new ArrayList<>();
+        String sql = "Select * From Account Where role=?";
+
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, role);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                list.add(new Account(
+                        rs.getString("account"),
+                        rs.getString("pass"),
+                        rs.getString("lastName"),
+                        rs.getString("firstName"),
+                        rs.getDate("birthday"),
+                        rs.getBoolean("gender"),
+                        rs.getString("phone"),
+                        rs.getBoolean("isUsed"),
+                        rs.getInt("roleInSystem")));
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     @Override
