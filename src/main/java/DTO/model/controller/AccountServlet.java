ea9db9d758a4f037f131d231993822992d7cb9ee
PoBo02 <paopaopeo0258@gmail.com>
2026-02-25 19:41:27 +0700

    add Dependency in pom.xml, creating a new AccountServlet

---
+++ src/main/java/DTO/model/controller/AccountServlet.java
@@ -0,0 +1,5 @@
+package DTO.model.controller;
+
+public class AccountServlet  {
+    
+}
--- src/main/java/DTO/model/dao/AccountDAO.java
+++ src/main/java/DTO/model/dao/AccountDAO.java
@@ -142,7 +142,7 @@ public class AccountDAO implements Accessable<Account> {
     @Override
     public Account getObjectById(String id) {
         String sql = "Select * From Account Where account= ?";
-        try (Connection con = new Connection();
+        try (Connection con = getConnection();
                 PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, id);
             ResultSet rs = ps.executeQuery();
@@ -156,12 +156,13 @@ public class AccountDAO implements Accessable<Account> {
                         rs.getString("phone"),
                         rs.getBoolean("isUsed"),
                         rs.getInt("roleInSystem"));
-x   
+  
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return null;
 
     }
 
@@ -192,11 +193,49 @@ String sql = "Select * From Account";
     }
 
     public int updateIsUsed(String acc, boolean isUsed) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "UPDATE Account SET isUsed=? WHERE account=?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setBoolean(1, isUsed);
+            ps.setString(2, acc);
+
+            int kq = ps.executeUpdate();
+
+            if (kq != 0) {
+                return kq;
+
+            } else {
+                throw new Exception();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     public Account loginSuccess(String acc, String pass) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "Select * From Account Where account=? and pass=?";
+        try (Connection con = getConnection();
+                PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, acc);
+            ps.setString(2, pass);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Account(rs.getString("account"),
+                        rs.getString("pass"),
+                        rs.getString("lastName"),
+                        rs.getString("firstName"),
+                        rs.getDate("birthday"),
+                        rs.getBoolean("gender"),
+                        rs.getString("phone"),
+                        rs.getBoolean("isUsed"),
+                        rs.getInt("roleInSystem"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
