bc2e92136e6fdcca8a3343371c1d32bdeb2fb560
PoBo02 <paopaopeo0258@gmail.com>
2026-02-26 12:58:30 +0700

    (ProductDAO): adding everyfunction fo the ProductDAO

--- src/main/java/DTO/model/dao/ProductDAO.java
+++ src/main/java/DTO/model/dao/ProductDAO.java
@@ -1,58 +1,196 @@
 package DTO.model.dao;
 
 import java.sql.Connection;
-import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletContext;
 
+import DTO.model.Account;
+import DTO.model.Category;
 import DTO.model.Product;
 import DTO.model.ultis.ConnectDB;
 
 public class ProductDAO implements Accessable<Product> {
+
     private ServletContext sc;
     private Connection con;
 
-    public ProductDAO() throws ClassNotFoundException, SQLException {
+    public ProductDAO() {
     }
 
-    public ProductDAO(ServletContext sc) throws ClassNotFoundException, SQLException {
+    public ProductDAO(ServletContext sc) {
         this.sc = sc;
     }
 
-    private Connection getConnection() throws SQLException,
-            ClassNotFoundException {
+    private Connection getConnection() throws Exception {
         con = new ConnectDB(sc).getConnection();
         return con;
     }
 
     @Override
-    public int insertRec(Product obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int insertRec(Product p) {
+        String sql = "INSERT INTO products "
+                + "(productId, productName, productImage, brief, typeId, account, unit, price, discount) "
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setString(1, p.getProductID());
+            ps.setString(2, p.getProductName());
+            ps.setString(3, p.getProductImage());
+            ps.setString(4, p.getBrief());
+            ps.setInt(5, p.getType().getTypeID());
+            ps.setString(6, p.getAccount().getAccount());
+            ps.setString(7, p.getUnit());
+            ps.setInt(8, p.getPrice());
+            ps.setInt(9, p.getDiscount());
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
-    public int updateRec(Product obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int updateRec(Product p) {
+        String sql = "UPDATE products SET "
+                + "productName=?, productImage=?, brief=?, typeId=?, "
+                + "unit=?, price=?, discount=? "
+                + "WHERE productId=?";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setString(1, p.getProductName());
+            ps.setString(2, p.getProductImage());
+            ps.setString(3, p.getBrief());
+            ps.setInt(4, p.getType().getTypeID());
+            ps.setString(5, p.getUnit());
+            ps.setInt(6, p.getPrice());
+            ps.setInt(7, p.getDiscount());
+            ps.setString(8, p.getProductID());
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
-    public int deleteRec(Product obj) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public int deleteRec(Product p) {
+        String sql = "DELETE FROM products WHERE productId=?";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setString(1, p.getProductID());
+            return ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Override
     public Product getObjectById(String id) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        String sql = "SELECT * FROM products WHERE productId=?";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setString(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Product p = new Product();
+                p.setProductID(rs.getString("productId"));
+                p.setProductName(rs.getString("productName"));
+                p.setProductImage(rs.getString("productImage"));
+                p.setBrief(rs.getString("brief"));
+                p.setPostedDate(rs.getDate("postedDate"));
+                p.setUnit(rs.getString("unit"));
+                p.setPrice(rs.getInt("price"));
+                p.setDiscount(rs.getInt("discount"));
+
+                Category c = new Category();
+                c.setTypeID(rs.getInt("typeId"));
+                p.setType(c);
+
+                Account a = new Account();
+                a.setAccount(rs.getString("account"));
+                p.setAccount(a);
+
+                return p;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
-    public java.util.List<Product> listAll() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Product> listAll() {
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM products";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                Product p = new Product();
+                p.setProductID(rs.getString("productId"));
+                p.setProductName(rs.getString("productName"));
+                p.setProductImage(rs.getString("productImage"));
+                p.setBrief(rs.getString("brief"));
+                p.setPostedDate(rs.getDate("postedDate"));
+                p.setUnit(rs.getString("unit"));
+                p.setPrice(rs.getInt("price"));
+                p.setDiscount(rs.getInt("discount"));
+
+                Category c = new Category();
+                c.setTypeID(rs.getInt("typeId"));
+                p.setType(c);
+
+                Account a = new Account();
+                a.setAccount(rs.getString("account"));
+                p.setAccount(a);
+
+                list.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 
     public List<Product> listByCategory(int categoryId) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+        List<Product> list = new ArrayList<>();
+        String sql = "SELECT * FROM products WHERE typeId=?";
 
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setInt(1, categoryId);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Product p = new Product();
+                p.setProductID(rs.getString("productId"));
+                p.setProductName(rs.getString("productName"));
+                p.setProductImage(rs.getString("productImage"));
+                p.setBrief(rs.getString("brief"));
+                p.setPostedDate(rs.getDate("postedDate"));
+                p.setUnit(rs.getString("unit"));
+                p.setPrice(rs.getInt("price"));
+                p.setDiscount(rs.getInt("discount"));
+
+                Category c = new Category();
+                c.setTypeID(categoryId);
+                p.setType(c);
+
+                Account a = new Account();
+                a.setAccount(rs.getString("account"));
+                p.setAccount(a);
+
+                list.add(p);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
